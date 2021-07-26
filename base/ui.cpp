6a83cdc65e1eff3af826be209dee4f1ac3fbbf82
Patrick Gordon <pgpatrickgordon@gmail.com>
2021-07-26 18:27:59 +0100

    misc imgui fixes

--- base/ui.cpp
+++ base/ui.cpp
@@ -466,7 +466,7 @@ bool UIOverlay::checkBox(const char* caption, int32_t* value) const {
 }
 
 bool UIOverlay::inputFloat(const char* caption, float* value, float step, uint32_t precision) const {
-    return ImGui::InputFloat(caption, value, step, step * 10.0f, precision);
+    return ImGui::InputFloat(caption, value, step, step * 10.0f, "%f");
 }
 
 bool UIOverlay::sliderFloat(const char* caption, float* value, float min, float max) const {
